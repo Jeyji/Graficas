878de728d4021c0cacdf8fb5b6a7cf14e8816b6d
Estudiante <Estudiante@10.203.1.59>
2017-05-04 16:03:24 -0500

    p3

--- src/graficas/TestPaintComponent.java
+++ src/graficas/TestPaintComponent.java
@@ -6,11 +6,15 @@
 package graficas;
 import java.awt.Color;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class TestPaintComponent extends JFrame {
+public class TestPaintComponent extends JFrame{
     public TestPaintComponent(){
         add(new NewPanel());
     }  
@@ -18,53 +22,97 @@ public class TestPaintComponent extends JFrame {
     public static void main(String[] args) {
         TestPaintComponent frame = new TestPaintComponent();
         frame.setTitle("TestPaintComponent");
-        frame.setSize(700,700);
+        frame.setSize(1024,520);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }      
+
+    
 }
 
-class NewPanel extends JPanel implements ActionListener{        
+class NewPanel extends JPanel implements ActionListener, MouseListener {        
     private Timer time;
-    public NewPanel() {
-        time = new Timer(25, this);
-        this.time.start();
-    }
+    private int x;
+
     
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Point p = e.getPoint();
+        if(getBounds().contains(p)){
+                time.stop();
+                time.start();
+            }
+        }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
         
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }  
+    private boolean checkCollisions() {
+        Rectangle Carro = this.getBounds();
+        Rectangle Muro = new Rectangle(400, 40, 100, 200);
+        boolean t = false;
+        if(Carro.intersects(Muro)){
+            System.out.println("Colision");
+            t = true;
+        }
+        return t;
+    } 
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillOval(40, 120, 40, 40);
-        g.fillOval(120, 120, 40, 40);
-        g.fillOval(260, 120, 40, 40);
-        g.fillOval(340, 120, 40, 40);
-        g.fillOval(580, 120, 40, 40);
-        g.fillOval(500, 120, 40, 40);
-    
-        g.drawRect(0, 20, 205, 160);
-        g.drawRect(210, 20, 220, 160);
-        g.drawRect(445, 20, 220, 160);
-            
-        g.setColor(Color.BLUE);
-        g.fillRect(0, 80, 200, 40);
-        int x[] = {80,120,160,40};
-        int y[] = {40,40,80,80};
-        g.fillPolygon(x, y, x.length);
-        g.fillRect(220, 80, 200, 40);
-        int z[] = {300,340,380,260};
-        int w[] = {40,40,80,80};
-        g.fillPolygon(z, w, x.length);
-        g.fillRect(460, 80, 200, 40);
-        int t[] = {540,580,620,500};
-        int p[] = {40,40,80,80};
-        g.fillPolygon(t, p, x.length);
-        System.out.println("click");
+        boolean t = checkCollisions();
+        Image fondo = loadImage("fondo.png");
+        g.drawImage(fondo, 0, 0, null);
+        int y = 50;
+        if(t==false){
+            g.fillRect(400, 380, 50, 40);
+            g.setColor(Color.BLUE);
+            g.fillRect(x+0, 380, 200, 40);
+        }else{
+            g.setColor(Color.BLUE);
+            g.fillRect(x+0-50, 380, 200+y, 40);
+        }
+        x++;
         }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        checkCollisions();
         repaint();
+    }
+    public NewPanel() {
+        time = new Timer(25, this);
+        this.time.start();
+        this.x = 10;
+        this.addMouseListener(this);
+        
+    }
+    @Override
+    public Rectangle getBounds(){
+        return new Rectangle (x-10, 20, 220, 160);
+    }
+
+    
+    public Image loadImage(String imageName){
+        ImageIcon ii = new ImageIcon("fondo.png");
+        Image image = ii.getImage();
+        return image;
     }
 }

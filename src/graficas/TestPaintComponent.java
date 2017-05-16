39a646822dc20c42e7783f978252069a7295ce28
Jeyji <galvizandres@hotmail.com>
2017-05-16 04:38:47 -0500

    Update TestPaintComponent.java

--- src/graficas/TestPaintComponent.java
+++ src/graficas/TestPaintComponent.java
@@ -34,10 +34,11 @@ public class TestPaintComponent extends JFrame{
 
 class NewPanel extends JPanel implements ActionListener, MouseListener {        
     private Timer time;
-    private int x,y;
-    int secuencia;
-    
-    
+    private int x,y,o,es,re,nub;
+    private final int ixc = 1200;
+    int secuencia, secuencias;
+    int up = 0;
+    private boolean g1,g2,g3;
     
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -68,88 +69,228 @@ class NewPanel extends JPanel implements ActionListener, MouseListener {
         
     }  
     private boolean checkCollisions() {
-        Rectangle Carro = this.getBounds();
-        Rectangle Muro = new Rectangle(400, 40, 100, 200);
+        Rectangle pie0 = this.getBounds();
+        Rectangle pie1 = this.get1();
+        Rectangle pie2 = this.get2();
+        Rectangle pie3 = this.get3();
+        Rectangle pie4 = this.get4();
+        Rectangle pie5 = this.get5();
+        Rectangle man1 = new Rectangle(0, 615+y, 100, 95);
         boolean t = false;
-        if(Carro.intersects(Muro)){
-            System.out.println("Colision");
+        if(man1.intersects(pie0)|| man1.intersects(pie1)||man1.intersects(pie2)||man1.intersects(pie3)||man1.intersects(pie4)||man1.intersects(pie5)){
+            System.out.println("colison");
             t = true;
         }
         return t;
     } 
+    private boolean G1(){
+        Rectangle coin1 = this.get6();
+        Rectangle man3 = new Rectangle(0, 615+y, 100, 95);
+        if(man3.intersects(coin1)){
+            g1 = true;
+        }
+        return g1;
+    }
+    private boolean G2(){
+        Rectangle coin2 = this.get7();
+        Rectangle man3 = new Rectangle(0, 615+y, 100, 95);
+        if(man3.intersects(coin2)){
+            g2 = true;
+        }
+        return g2;
+    }
+    private boolean G3(){
+        Rectangle coin3 = this.get8();
+        Rectangle man3 = new Rectangle(0, 615+y, 100, 95);
+        if(man3.intersects(coin3)){
+            g3 = true;
+        }
+        return g3;
+    }
+    private boolean checkCollision(){
+        Rectangle pie0 = this.getBounds();
+        Rectangle pie1 = this.get1();
+        Rectangle pie2 = this.get2();
+        Rectangle pie3 = this.get3();
+        Rectangle pie4 = this.get4();
+        Rectangle pie5 = this.get5();
+        Rectangle man2 = new Rectangle(0, 615+y, 100, 100);
+        boolean fl = false;
+        if(man2.intersects(pie0)|| man2.intersects(pie1)||man2.intersects(pie2)||man2.intersects(pie3)||man2.intersects(pie4)||man2.intersects(pie5)){
+            System.out.println("colison");
+            fl = true;
+        }
+        return fl;
+    }
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
         boolean t = checkCollisions();
+        boolean fl = checkCollision();
         Image fondo = loadImage("blue_background.png");
         Image nube = loadImage("clouds.png");
         Image rojo = loadImage("red_background.png");
         for(x=0; x<50;x++){
-            g.drawImage(fondo, x*22, 0, null);           
+            g.drawImage(fondo, (x*22)-es, 0, null);           
         }
         for(x=50; x<100;x++){
-            g.drawImage(rojo, x*22, 0, null);
+            g.drawImage(rojo, (x*22)-es, 0, null);
+        }
+        for(x=100; x<150;x++){
+            g.drawImage(fondo, (x*22)-es, 0, null);           
+        }
+        for(x=-50; x<0;x++){
+            g.drawImage(rojo, (x*22)-es, 0, null);
         }
         for(x=0; x<50;x++){
-            g.drawImage(nube, x*335, 50, null);
+            g.drawImage(nube, (x*335)-es, 50, null);
         }
         Image tierra = loadImage("ground_loop.png");
         for(x=0; x<50;x++){
-            g.drawImage(tierra, x*2*110, 710, null);           
+            g.drawImage(tierra, (x*2*110)-es, 710, null);           
         }
         Image tierr = loadImage("ground_single.png");
         for(x=1; x<50;x++){
-            g.drawImage(tierr, x*110, 710, null);           
+            g.drawImage(tierr, (x*110)-es, 710, null);           
         }
         Image pie = loadImage("stone.png");
-        g.drawImage(pie, 250, 655, null);
-        g.drawImage(pie, 500, 655, null);
-        g.drawImage(pie, 565, 655, null);
-        g.drawImage(pie, 630, 655, null);
-        g.drawImage(pie, 565, 600, null);
-        g.drawImage(pie, 630, 600, null);
+        g.drawImage(pie, 250-es, 655, null);
+        g.drawImage(pie, 500-es, 655, null);
+        g.drawImage(pie, 565-es, 655, null);
+        g.drawImage(pie, 630-es, 655, null);
+        g.drawImage(pie, 565-es, 600, null);
+        g.drawImage(pie, 630-es, 600, null);
         Image coin = loadImage("coin.png");
-        g.drawImage(coin, 250, 600, null);
-        g.drawImage(coin, 500, 600, null);
-        g.drawImage(coin, 570, 545, null);
-        g.drawImage(coin, 635, 545, null);
-        Image ene = loadImage("enemy_standing.png");
-        g.drawImage(ene, 422, 615, 320, 716, this.secuencia*102, 0, (this.secuencia*102)+102, 100, this);
-        int o = 0;
-        while(o < 50){
-            Image run = loadImage("enemy_run.png");
-            g.drawImage(run, 1200-y, 615, 1305-y, 716, this.secuencia*(105-y), 0, this.secuencia*(105-y)+(105-y), 101, this);
-            o++;
+        g1=G1();
+        g2=G2();
+        g3=G3();
+        if(g1==false){
+            g.drawImage(coin, 250-es, 600, null);
         }
+        if(g2==false){
+            g.drawImage(coin, 500-es, 600, null);
+        }
+        if(g3==false){
+            g.drawImage(coin, 635-es, 545, null);
+        }
+        Image ene = loadImage("enemy_standing.png");
+        g.drawImage(ene, 422-es, 615, 320-es, 716, this.secuencia*102, 0, (this.secuencia*102)+102, 100, this);
+        Image run = loadImage("enemy_run.png");
+        g.drawImage(run, o-es, 615 ,o + 105-es,716,(this.secuencia*105),0,(this.secuencia*105)+105,101,this);
+        Image man = loadImage("standing.png");
+        Image wal = loadImage("walking.png");
+        Image sup = loadImage("jump.png");
+        g.drawRect(0, 615+y, 100, 95);
+        g.drawRect(0, 615+y, 100, 100);
+        g.drawRect(0-es, 716, 2000, 400);
+        int co = 0;
+        if(up==0){    
+            g.drawImage(man, 0, 615+y, 100, 715+y, this.secuencia*119, 0, (this.secuencia*119)+119, 127, this);
+        }else if(up==1){
+            g.drawImage(wal, 0, 615+y, 100, 715+y, this.secuencias*119, 0, (this.secuencias*119)+119, 127, this);
+            if(fl==false && co == 0){    
+                y += 1;
+                if (re == 1){    
+                    es += 1;
+                }
+                g.drawImage(sup, 0, 615+y, 85, 110, null);
+                fl = checkCollision();
+                if(fl==true){
+                    fl=false;
+                    up = 0;
+                    co = 1;
+                }
+            }
+        }else if(up == 2){
+            g.drawImage(sup, 0, 615+y, 85, 110, null);
+        }else if(up==4){
+            g.drawImage(sup, 0, 615+y, 85, 110, null);
+            if(fl==false && co == 0){
+                y += 1;
+                if (re == 1){    
+                    es += 1;
+                }
+                fl = checkCollision();
+                if(fl==true){
+                    y -= 1;
+                    fl=false;
+                    co = 1;
+                    up = 0;
+                    nub = 0;
+                }
+            }
+        }
+        g.drawRect(900, 100, 150, 40);
+        g.drawString("COINS", 910, 125);
+        if(g1 == true && g2 == true && g3 == true ){
+            g.drawString("3", 980 , 125);
+        }else if(g1== true && g2 ==true){
+            g.drawString("2", 980 , 125);
+        }else if(g1== true){
+            g.drawString("1", 980, 125);
+        }else{
+            g.drawString("0", 980, 125);
+        }
+        repaint();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         checkCollisions();
-        y += 20;
+        o -= 20;
         if(this.secuencia == 1){
             this.secuencia = 0;
         }else{
             this.secuencia++;
         }
+        if(this.secuencias == 3){
+            this.secuencias = 0;
+        }else{
+            this.secuencias++;
+        }
+        if(o<=700)
+            o = ixc;
         repaint();
     }
     public NewPanel() {
+        
         time = new Timer(1000, this);
         this.time.start();
-        this.x = 10;
+        this.o = ixc;
         this.addMouseListener(this);
-        
         addKeyListener(new TAdapter());
         setFocusable(true);
         
     }
-
     @Override
     public Rectangle getBounds(){
-        return new Rectangle (x-10, 20, 220, 160);
+        return new Rectangle (250-es, 655, 315-es, 714);
     }
-
+    public Rectangle get1(){
+        return new Rectangle (500-es, 655, 565-es, 714);
+    }
+    public Rectangle get2(){
+        return new Rectangle (630-es, 655, 695-es, 714);
+    }
+    public Rectangle get3(){
+        return new Rectangle (565-es, 600, 630-es, 655);
+    }
+    public Rectangle get4(){
+        return new Rectangle (630-es, 600, 695-es, 655);
+    }
+    public Rectangle get5(){
+        return new Rectangle (0-es, 716, 2000, 200);
+    }
+    public Rectangle get6(){
+        return new Rectangle (250-es, 600, 55, 55);
+    }
+    public Rectangle get7(){
+        return new Rectangle (500-es, 600, 55, 55);
+    }
+    public Rectangle get8(){
+        return new Rectangle (635-es, 545, 55, 55);
+    }
+    
     
     public Image loadImage(String imageName){
         ImageIcon ii = new ImageIcon(imageName);
@@ -162,24 +303,42 @@ class NewPanel extends JPanel implements ActionListener, MouseListener {
 
         @Override
         public void keyReleased(KeyEvent e){
+            up = 4;
+            y -= 1;
+            boolean fl = checkCollision();
         }
         @Override
         public void keyPressed(KeyEvent e){
+            re = 0;
+            boolean t = checkCollisions();
+            boolean fl = checkCollision();
             int key = e.getKeyCode();
             if(key == KeyEvent.VK_SPACE){
                 System.out.println("Espacio");
             }
             if(key == KeyEvent.VK_UP){
-                y -=10;
+                if(fl==false){
+                    es += 1;
+                }
+                if(nub<-200){
+                    up=4;
+                }else{
+                    nub -= 5;
+                    y -= 5;
+                    up=2;
+                }
+                re = 1;
+                
             }
             if(key == KeyEvent.VK_RIGHT){
-                x +=10;
-            }
-            if(key == KeyEvent.VK_DOWN){
-                y +=10;
+                if(t==false){
+                    es += 1;
+                }
+                up = 1;
             }
             if(key == KeyEvent.VK_LEFT){
-                x -=10;
+                es -=1;
+                up = 1;
             }
         }
     }
